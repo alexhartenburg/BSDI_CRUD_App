bea7479aebd64dcb90d4276bead027f2822718ca
Alex Hartenburg <alexhartenburg@Allysons-MacBook-Air.local>
2022-05-25 05:12:31 -0700

    Added two migrations for users and posts.

---
+++ server/migrations/20220525120341_create_posts.js
@@ -0,0 +1,27 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+ exports.up = function(knex) {
+    return knex.schema.createTable("posts", (table) => {
+        table.increments();
+        table.string("user_id", 50);
+        table.foreign("user_id").references("users.id");
+        table.string("title", 50);
+        table.string("content", 50);
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema
+    .alterTable("posts", (table) => {
+      table.dropForeign("user_id");
+    })
+    .then(function () {
+      return knex.schema.dropTableIfExists("posts");
+    });
+};
